6ea0a12060749d613b840453ca4d268a015d5ee8
istiyakaminsanto <istiyakaminsanto@gmail.com>
2018-01-02 01:23:13 +0600

    motamuti compleate

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -327,7 +327,7 @@ jQuery(function ($) {
     // -------------------------------------------------------------
 
     (function () {
-        var myLatlng = new google.maps.LatLng(41.372641, -74.687387);
+        var myLatlng = new google.maps.LatLng(22.3592646, 91.8010708);
 
             var styles = [
                 {
